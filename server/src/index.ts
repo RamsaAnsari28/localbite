c173a1085aedbd698ba510cbbe37c1a930618b2c
Ramsa Ansari <ansariramsha05@gmail.com>
2026-09-12 13:31:06 +0530

    Prepare backend for deployment

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,7 @@ import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 
-const PORT = 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
 app.use(cors());
 app.use(express.json());
